3c1fb2816430e2127a4baea71763cb3909a5dc1e
Mateus Lorandi dos Santos <mcomogo@gmail.com>
2014-09-15 00:49:56 -0300

    First commit

---
+++ Gemfile
@@ -0,0 +1,4 @@
+#!/usr/bin/env ruby
+source 'http://rubygems.org'
+
+gem 'jekyll', '2.4.0'
---
+++ assets/javascripts/main.js
@@ -0,0 +1,5 @@
+(function() {
+  $('#pager li.disabled a').on('click', function(event) {
+    return false;
+  })
+})();
